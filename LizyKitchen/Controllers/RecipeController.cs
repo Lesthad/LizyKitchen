42835cd491eca8af0808634386554c3666218ab1
Mike P <mperaza_7@hotmail.com>
2018-02-25 01:12:36 -0600

    Adding the Recipe model, controller and view as well as the .gitignore file
    Adding the Recipe model, controller and view as well as the .gitignore file

---
+++ LizyKitchen/Controllers/RecipeController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using LizyKitchen.App_Start;
+using MongoDB.Bson;
+using MongoDB.Driver;
+
+namespace LizyKitchen.Controllers
+{
+    public class RecipeController : Controller
+    {
+        public readonly JalapenioContext Context = new JalapenioContext();
+
+        public ActionResult Index()
+        {
+            var recipies = Context.Recipes.Find(new BsonDocument()).ToEnumerable();
+
+            return View(recipies);
+        }
+    }
+}
---
+++ LizyKitchen/Models/Recipe.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace LizyKitchen.Models
+{
+    public class Recipe
+    {
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string Id { get; set; }
+
+        [Required]
+        public string Title { get; set; }
+        public string ShortDescription { get; set; }
+        public string CookTime { get; set; }
+        public string Level { get; set; }
+
+        [Required]
+        [Display(Name = "Modo de Preparacion 1")]
+        public string LongDescription1 { get; set; }
+
+        [Display(Name = "Modo de Preparacion 2")]
+        public string LongDescription2 { get; set; }
+
+        [Display(Name = "Modo de Preparacion 3")]
+        public string LongDescription3 { get; set; }
+
+        [Required]
+        [Display(Name = "Photo Url")]
+        public string ImageUrl { get; set; }
+
+        public Recipe()
+        {
+
+        }
+
+        /*public Recipe(PostRecipe postRecipe)
+        {
+            Title = postRecipe.Title;
+            ShortDescription = postRecipe.ShortDescription;
+            LongDescription1 = postRecipe.LongDescription1;
+            LongDescription2 = postRecipe.LongDescription2;
+            LongDescription3 = postRecipe.LongDescription3;
+            ImageUrl = postRecipe.ImageUrl;
+        }*/
+    }
+}
