d5524ac7fb3ca7fcd10c5de194b888f554fae7ba
Mike P <mperaza_7@hotmail.com>
2018-03-08 23:14:28 -0600

    3th checking
    Small corrections, changing the default route to be Recipe, fixing the defualt style for the textboxes

--- LizyKitchen/Models/PostRecipe.cs
+++ LizyKitchen/Models/PostRecipe.cs
@@ -1,14 +1,29 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
+
 namespace LizyKitchen.Models
 {
     public class PostRecipe
     {
+        [Required]
+        [Display(Name = "Titulo")]
         public string Title { get; set; }
+
+        [Required]
+        [Display(Name = "Descripcion Corta")]
         public string ShortDescription { get; set; }
         public string CookTime { get; set; }
         public string Level { get; set; }
+
+        [Required]
+        [Display(Name = "Modo de Preparacion 1")]
         public string LongDescription1 { get; set; }
+
+        [Required]
+        [Display(Name = "Modo de Preparacion 2")]
         public string LongDescription2 { get; set; }
+
+        [Display(Name = "Modo de Preparacion 3")]
         public string LongDescription3 { get; set; }
         public string ImageUrl { get; set; }
     }
